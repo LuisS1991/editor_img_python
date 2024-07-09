fe03eecb9aa20fa081724201e7688494766cfce6
Luis <112443531+LuisS1991@users.noreply.github.com>
2024-07-09 12:52:09 -0300

    Update FileHelper.py

--- uitles/FileHelper.py
+++ uitles/FileHelper.py
@@ -4,7 +4,7 @@ import os
 
 def seleccionarImagen():
     ruta_imge = filedialog.askopenfile(
-        initialdir="/home/jpmontevideo/Descargas",
+        initialdir="/home/user/Descargas",
         filetypes=[("image", ".jpg"), ("image", ".jpeg"), ("image", ".png")],
     )
     if len(ruta_imge.name) > 0:
@@ -15,14 +15,14 @@ def seleccionarImagen():
 
 def rutaSeleccionada():
     guardarimg = filedialog.asksaveasfilename(
-        initialdir="/home/jpmontevideo/Descargas",
+        initialdir="/home/user/Descargas",
         title="Seleccione el archivo",
         defaultextension=".jpeg",
         filetypes=(("jpeg files", "*.jpeg"), ("all files", "*.*")),
     )
     if len(guardarimg) > 0:
         return guardarimg
-    return "/home/jpmontevideo/Descargas/"
+    return "/home/user/Descargas/"
 
 
 def eliminar_temporales(ruta):
